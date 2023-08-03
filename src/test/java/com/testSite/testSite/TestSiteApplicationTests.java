3eeba40e86a78370305d5bd1ebe8a066891ce934
box03 <box03-853@yandex.ru>
2023-08-03 12:18:10 +0400

    Первый коммит

---
+++ src/test/java/com/testSite/testSite/TestSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testSite.testSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
